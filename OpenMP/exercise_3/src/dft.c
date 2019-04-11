6f3b4628b27fe252367085b2e9a3e4e6f192bda8
achistef <achste@beskow-login2.pdc.kth.se>
2019-04-11 12:41:58 +0200

    implemented exercise 3

--- OpenMP/exercise_3/src/dft.c
+++ OpenMP/exercise_3/src/dft.c
@@ -4,21 +4,26 @@
 // idft: 1 direct DFT, -1 inverse IDFT (Inverse DFT)
 int DFT(int idft, double* xr, double* xi, double* Xr_o, double* Xi_o, int N)
 {
-	for (int k = 0 ; k < N; k++) {
-		for (int n = 0; n < N; n++) {
-			// Real part of X[k]
-			Xr_o[k] += xr[n] * cos(n * k * PI2 / N) + idft * xi[n] * sin(n * k * PI2 / N);
-			// Imaginary part of X[k]
-			Xi_o[k] += -idft * xr[n] * sin(n * k * PI2 / N) + xi[n] * cos(n * k * PI2 / N);
+	#pragma omp parallel
+	{
+		#pragma omp for collapse(2)
+		for (int k = 0 ; k < N; k++) {
+			for (int n = 0; n < N; n++) {
+				#pragma omp atomic
+				Xr_o[k] += xr[n] * cos(n * k * PI2 / N) + idft * xi[n] * sin(n * k * PI2 / N);
+				#pragma omp atomic
+				Xi_o[k] += -idft * xr[n] * sin(n * k * PI2 / N) + xi[n] * cos(n * k * PI2 / N);
+			}
 		}
-	}
-		
-	// normalize if you are doing IDFT
-	if (idft == -1){
-		for (int n = 0; n < N; n++) {
-			Xr_o[n] /= N;
-			Xi_o[n] /= N;
-		}
+	 	
+		// normalize if you are doing IDFT
+        	if (idft == -1){
+			#pragma omp for
+        	        for (int n = 0; n < N; n++) {
+                	        Xr_o[n] /= N;
+        	                Xi_o[n] /= N;
+        	        }
+       		}	
 	}
 	return 1;
 }
