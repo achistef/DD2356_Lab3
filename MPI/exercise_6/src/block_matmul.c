b9f5331d65f65e3837f9b400a292c48810873691
achistef <achste@kth.se>
2019-05-08 17:17:19 +0200

    updated ex6

--- MPI/exercise_6/src/block_matmul.c
+++ MPI/exercise_6/src/block_matmul.c
@@ -12,14 +12,14 @@ struct Config {
 	double *A, *A_tmp, *B, *C;
 
 	/* Cart communicators */
-	MPI_Comm grid_comm;
-	MPI_Comm row_comm;
-	MPI_Comm col_comm;
+	MPI_Comm grid_comm;				//set
+	MPI_Comm row_comm;				//set
+	MPI_Comm col_comm;				//set
 
 	/* Cart communicator dim and ranks */
-	int dim[2], coords[2];
-	int world_rank, world_size, grid_rank;		//set 1,2
-	int row_rank, row_size, col_rank, col_size;
+	int dim[2], coords[2];				//set
+	int world_rank, world_size, grid_rank;		//set
+	int row_rank, row_size, col_rank, col_size;	//set
 
 	/* Full matrix dim */
 	int A_dims[2]; 					//set
@@ -36,7 +36,7 @@ struct Config config;
 
 void init_matmul(char *A_file, char *B_file, char *outfile)
 {
-	printf("init matmul...\n");
+
 	MPI_Comm_rank(MPI_COMM_WORLD, &config.world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &config.world_size);
 
@@ -64,7 +64,6 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 
 	/* Set dim of tiles relative to the number of processes as NxN where N=sqrt(world_size) */
 	config.dim[0] = config.dim[1] = sqrt(config.world_size);
-	printf("rank %d: my dim is %d", config.world_rank, config.dim[0]);
 
 
 	/* Verify dim of A and B matches for matul and both are square*/
@@ -79,22 +78,52 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 	if(config.A_dims[1] != config.B_dims[0]){
 		MPI_Finalize();
 	}
+	
+	if(config.A_dims[0] % config.dim[0] != 0 || config.A_dims[1] % config.dim[1] != 0 || config.B_dims[0] % config.dim[0] != 0 || config.B_dims[1] % config.dim[1] != 0){
+		MPI_Finalize();
+	}
 
 
 	/* Create Cart communicator for NxN processes */
 	int period[2] = {1, 1};
 	MPI_Cart_create(MPI_COMM_WORLD, 2, config.dim, period , 1, &config.grid_comm);
+
+	MPI_Cart_coords(config.grid_comm, config.world_rank, 2, config.coords);
+        //printf("Rank %d coordinates are %d %d\n", config.world_rank, config.coords[0], config.coords[1]);
+
+	MPI_Cart_rank(config.grid_comm, config.coords, &config.grid_rank);
+        //printf("The processor at position (%d, %d) has rank %d\n", config.coords[0], config.coords[1], config.grid_rank);
 	
 
 	/* Sub div cart communicator to N row communicator */
+	int selected_dim[2] = {1, 0};
+	MPI_Cart_sub(config.grid_comm, selected_dim, &config.row_comm);
+	MPI_Comm_rank(config.row_comm, &config.row_rank);
+	MPI_Comm_size(config.row_comm, &config.row_size);
+	//printf("Rank %d: Row comm rank %d, world size %d\n", config.world_rank, config.row_rank, config.row_size);
+
 
 	/* Sub div cart communicator to N col communicator */
+	selected_dim[0] = 0;
+	selected_dim[1] = 1;
+        MPI_Cart_sub(config.grid_comm, selected_dim, &config.col_comm);
+	MPI_Comm_rank(config.col_comm, &config.col_rank);
+        MPI_Comm_size(config.col_comm, &config.col_size);
+	//printf("Rank %d: Col comm rank %d, world size %d\n", config.world_rank, config.col_rank, config.col_size);
+	
 
 	/* Setup sizes of full matrices */
+	config.C_dims[0] = config.A_dims[0];
+	config.C_dims[1] = config.B_dims[1];
+	config.matrix_size = config.C_dims[0] * config.C_dims[1];
 
 	/* Setup sizes of local matrix tiles */
+	config.local_dims[0] = config.local_dims[1] = config.C_dims[0] / config.dim[0];
+	config.local_size = config.local_dims[0] * config.local_dims[1];
+
 
 	/* Create subarray datatype for local matrix tile */
+	
 
 	/* Create data array to load actual block matrix data */
 
@@ -107,7 +136,7 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 
 void cleanup_matmul()
 {
-	printf("cleanup...\n");
+	printf(" ");
 	/* Rank zero writes header specifying dim of result matrix C */
 
 	/* Set fileview of process to respective matrix block with header offset */
@@ -119,7 +148,7 @@ void cleanup_matmul()
 
 void compute_fox()
 {
-	printf("compute fox...\n");
+	printf(" ");
 
 	/* Compute source and target for verticle shift of B blocks */
 	int i;
